080cb28a749f3f88d48e59b63c5653b1e164888b
Tushar saxena <46051734+tsaxena4k@users.noreply.github.com>
2020-10-16 22:53:00 +0530

    Changed Debugging mode
    Hey ! changed the show debugging mode.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,  //Changed debugging mode
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
